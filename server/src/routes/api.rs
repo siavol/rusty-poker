7f6a612ab19b30f440b8da80362c540a579f17a3
Ilya Shestakov <siavol@gmail.com>
2022-10-06 11:48:20 +0300

    Add unit test for post session

--- server/src/routes/api.rs
+++ server/src/routes/api.rs
@@ -1,6 +1,5 @@
-use actix_web::{web::{ServiceConfig, scope}, post, Responder, HttpResponse, http::header::ContentType};
+use actix_web::{web::{ServiceConfig, scope, self}, Responder, HttpResponse, http::header::ContentType};
 
-#[post("/session")]
 async fn create_session() -> impl Responder {
     HttpResponse::Ok()
         .content_type(ContentType::json())
@@ -9,6 +8,27 @@ async fn create_session() -> impl Responder {
 
 pub fn config_service(cfg: &mut ServiceConfig) {
     cfg.service(scope("/api")
-        .service(create_session)
+        .route("/session", web::post().to(create_session))
     );
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{test, Responder, http, body::to_bytes};
+
+    use super::create_session;
+
+    #[actix_web::test]
+    async fn test_post_session_ok() {
+        let req = test::TestRequest::default()
+            .to_http_request();
+        let res = create_session().await.respond_to(&req);
+        assert_eq!(res.status(), http::StatusCode::OK);
+        
+        let body = match to_bytes(res.into_body()).await {
+            Ok(body) => body,
+            Err(_) => panic!("Can not get response body.")
+        };
+        assert_eq!(std::str::from_utf8(&body).unwrap(), r#"{ "name": "test", "id": "qwe123" }"#);
+    }
 }
